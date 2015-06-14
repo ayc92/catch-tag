253be347483500cd17f76105e85ae5776f2f1ff7
Jason Chen <jasonschen5@gmail.com>
2015-06-14 01:30:38 -0700

    initial commit for js

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
+from flask import request, send_from_directory
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def dashboard_view():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
