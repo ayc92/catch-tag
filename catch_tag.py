05bf78ae909bcdb334553bc2218cfdd2328f6f8d
Andrew Chen <aychen92@gmail.com>
2015-06-21 14:46:37 -0700

    Add script to get woeids, and add trend and search services for Twitter client.

--- catch_tag.py
+++ catch_tag.py
@@ -1,12 +1,24 @@
+import json
 from flask import Flask
 from flask import render_template
 
 application = Flask(__name__)
+
+WOEIDS = None
 
 
 @application.route('/')
 def dashboard_view():
     return render_template('index.html')
 
+
+def parse_woeids_file():
+    with open('supernames.json', 'r') as json_file:
+        data = json.load(json_file)
+    return data.keys()
+
+
 if __name__ == '__main__':
+    # Parse supernames.json to get a list of woeids.
+    WOEIDS = parse_woeids_file()
     application.run()
--- services/twitter.py
+++ services/twitter.py
@@ -1,9 +1,13 @@
 from base64 import b64encode
-import logging
 import requests
 from urllib import quote_plus
 
-logger = logging.Logger()
+
+class TwitterServiceException(Exception):
+    """
+    A subclass of a vanilla python Exception for more descriptive error reporting.
+    """
+    pass
 
 
 class TwitterAPIClient(object):
@@ -15,11 +19,12 @@ class TwitterAPIClient(object):
     # consumer key and secret is that they could make requests to Twitter on behalf
     # of our application, and potentially cause our application to reach its rate limit.
     TWITTER_CONSUMER_KEY = '7MY8c3BpeA95xNitXp7kNS9nU'
-    TWITTER_CONSUMER_SECRET = 'L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg'
+    TWITTER_CONSUMER_SECRET = 'QCURvwq3Nrgk8xyinBf7iZO25Tn0GVtiM0UO0rZ0osLmUlJGMw'
 
     # Twitter endpoints
     TWITTER_TOKEN_URL = 'https://api.twitter.com/oauth2/token'
     TWITTER_SEARCH_TWEET_URL = 'https://api.twitter.com/1.1/search/tweets.json'
+    TWITTER_TRENDS_PLACE_URL = 'https://api.twitter.com/1.1/trends/place.json'
 
     def _get_base_64_bearer_token_creds(self):
         return b64encode('%s:%s' % (quote_plus(self.TWITTER_CONSUMER_KEY), quote_plus(self.TWITTER_CONSUMER_SECRET)))
@@ -39,17 +44,37 @@ class TwitterAPIClient(object):
         response_dict = response.json()
         if response_dict['token_type'] != 'bearer':
             # This really shouldn't ever happen.
-            logger.error('For some reason, the Twitter OAuth2 token endpoint returned the wrong type of token.')
+            raise TwitterServiceException("For some reason, the Twitter OAuth2 token"
+                                          "endpoint returned the wrong type of token.")
         return response_dict['access_token']
 
     def __init__(self):
         """
         Get the Twitter application-only access token on instance creation.
         """
-        self.access_token = self._get_bearer_token()
+        access_token = self._get_bearer_token()
+        self.authorization_header = { 'Authorization': 'Bearer %s' % access_token }
 
-    def search_tweets(self, hashtag):
+    def get_twitter_trends(self, woeid):
         """
-        Search tweets filtered by a hashtag parameter.
+        Get the names of trending topics using the given woeid.
         """
-        pass
+        response = requests.get(
+            self.TWITTER_TRENDS_PLACE_URL, headers=self.authorization_header, params={ 'id': woeid })
+        response_json = response.json()
+        trends = response_json['trends']
+        search_queries = [trend['name'] for trend in trends]
+        return search_queries
+
+    def get_tweet_search_results(self, param):
+        """
+        Get the tweets associated with the given query parameter.
+        """
+        # URL-encode the search parameter.
+        esc_param = quote_plus(param)
+        response = requests.get(
+            self.TWITTER_SEARCH_TWEET_URL, headers=self.authorization_header, params={ 'q': esc_param })
+        response_json = response.json()
+        tweets = response_json['statuses']
+        map_id_to_tweet = { tweet['id_str']: tweet['text'] for tweet in tweets }
+        return map_id_to_tweet
